a72e4e42d1747df0141317880c480b7097df1a52
Behnam Amiri <behamiridev@gmail.com>
2023-11-06 16:56:54 -0700

    Add "createRandomEmail" and "createEmailFromNames"

--- src/person.js
+++ src/person.js
@@ -1,3 +1,4 @@
+const domains = require('../lib/consts/emailDomains');
 const femaleFirstNames = require('../lib/consts/femaleFirstName');
 const lastNames = require('../lib/consts/lastName');
 const maleFirstNames = require('../lib/consts/maleFirstName');
@@ -13,7 +14,7 @@ const {
  * @returns {string} A random first name
  */
 const firstName = (gender) => {
-  const int = giveMeRandomInt(0, 1499);
+  const int = giveMeRandomInt(0, maleFirstNames.length - 1);
 
   if (gender === 1) {
     return maleFirstNames[int];
@@ -77,7 +78,59 @@ const nameObjectOrArray = (gender, isArray, size) =>
   generateObjectOrArray(() => name(gender), isArray, size);
 
 
+/**
+ * Creates an email address from the given first and last names.
+ *
+ * @param {string} firstName - The first name.
+ * @param {string} lastName - The last name.
+ * @param {string} [nameOrder='f'] - The order in which the first and last names should be used ('f' for firstName first or 'l' for lastName first).
+ *
+ * @throws {Error} Throws an error if either first name or last name is missing, or if an invalid 'nameOrder' value is provided.
+ *
+ * @returns {string} The generated email address.
+ */
+const createEmailFromNames = (firstName, lastName, nameOrder = 'f') => {
+  if (!firstName || !lastName) {
+    throw new Error('Both first and last names are required.');
+  }
+
+  if (nameOrder === 'f') {
+    return `${firstName}.${lastName}@gmail.com`;
+  } else if (nameOrder === 'l') {
+    return `${lastName}.${firstName}@gmail.com`;
+  } else {
+    throw new Error('Invalid "nameOrder" value. Use "f" or "l".');
+  }
+}
+
+
+/**
+ * Generates a random email address based on optional gender-specific names.
+ *
+ * @param {string} [gender] - An optional parameter representing the gender. Use 1 for male and 0 for female.
+ *
+ * @throws {Error} Throws an error if an invalid gender value is provided.
+ *
+ * @returns {string} The generated random email address.
+ */
+const createRandomEmail = (gender) => {
+  const randomInt = giveMeRandomInt(0, domains.length - 1);
+  const randomName = {
+    firstName: firstName(gender).replace(/\s/g, ''),
+    lastName: lastName().replace(/\s/g, '')
+  };
+
+  const email = Math.random() < 0.5
+    ? `${randomName.firstName}.${randomName.lastName}@${domains[randomInt]}`
+    : `${randomName.lastName}.${randomName.firstName}@${domains[randomInt]}`;
+
+  return email;
+}
+
+
 module.exports = {
+  createEmailFromNames,
+  createRandomEmail,
   firstName,
   firstNameObjectOrArray,
   lastName,
